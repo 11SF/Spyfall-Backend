d495d7d67a57f24076c4b2d6c375a2198c09e094
11SF <nsfolknsf@gmail.com>
2022-09-02 01:33:08 +0700

    add new error type

--- utility/error.js
+++ utility/error.js
@@ -2,10 +2,20 @@ class InternalError extends Error {
     constructor(codeLevel, message) {
         super(codeLevel, message);
 
-        this.name = 'Internal Server Error';
+        this.name = 'Internal Server Error.';
         this.status = codeLevel;
         this.message = message.toString()
     }
 }
 
-module.exports = { InternalError }
+class InvalidDataError extends Error {
+    constructor(codeLevel, message) {
+        super(codeLevel, message);
+
+        this.name = 'Request Data invalid.';
+        this.status = codeLevel;
+        this.message = message.toString()
+    }
+}
+
+module.exports = { InternalError, InvalidDataError }

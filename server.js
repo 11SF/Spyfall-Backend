8684963e2646dc121429a63ce108d37b77c54065
11SF <nsfolknsf@gmail.com>
2022-09-07 04:13:11 +0700

    add cors

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
-const db = require("./db");
+const cors = require('cors')
 
 require('dotenv').config()
 const PORT = process.env.SERVER_PORT;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+require("./db");
 require("./routers")(app)
 const server = app.listen(PORT, () => {
     console.log("[success] task 1 : Server running on port " + PORT)

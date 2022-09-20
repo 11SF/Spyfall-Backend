b6f3d16e5f1754a409558eacf36b3aba6940110f
11SF <nsfolknsf@gmail.com>
2022-09-20 21:40:26 +0700

    edit socket event message and implement logic

--- socket/room.socket.js
+++ socket/room.socket.js
@@ -6,20 +6,87 @@ const { buildResponse } = require("../utility/response");
 module.exports = function (io) {
   io.sockets.on("connection", function (socket) {
     console.log("connected");
-    socket.on("createRoom", async (data) => {
+    socket.on("OWNER:CREATE-ROOM", async (data) => {
       try {
         let roomObject = mapRequestBodyRoomToModelObject(data);
-        console.log(roomObject);
         let result = await GameRoomService.create(roomObject);
-        console.log(result);
-        socket.join(result._id);
-        socket.emit("userJoinedMsg", buildResponse(result));
+        socket.emit("ROOM:SYS-MESSAGE", buildResponse(result));
       } catch (err) {
-        socket.emit("roomMsg", buildResponse(new InternalError(5080, err)));
+        socket.emit(
+          "ROOM:SYS-MESSAGE",
+          buildResponse(new InternalError(5080, err))
+        );
       }
-      // socket.to(result._id).emit("userJoinedMsg", data.userId + " เข้ามาแล้ววววว");
     });
-    socket.on("joinRoom", async (data) => {
+    // Room Setting by Owner
+    socket.on("OWNER:SETTING", async (data) => {
+      try {
+        let roomObject = mapRequestBodyRoomToModelObject(data);
+        let result = await GameRoomService.update(roomObject);
+        socket
+          .to(result._id)
+          .emit("ROOM:SYNC-SETTING", buildResponse(roomObject));
+      } catch (err) {
+        socket.emit(
+          "ROOM:SYS-MESSAGE",
+          buildResponse(new InternalError(5080, err))
+        );
+      }
+    });
+    socket.on("OWNER:START-GAME", async (data) => {
+      try {
+        //Step 1 : Random Role to player
+        let roles = await randomRoleToPlayer();
+
+        let timer = data.minutes * 60,
+          duration = data.minutes * 60,
+          minutes,
+          seconds;
+
+        //random location & roles
+        setInterval(function () {
+          minutes = parseInt(timer / 60, 10);
+          seconds = parseInt(timer % 60, 10);
+
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
+          socket.to(data.roomId).emit(
+            "INGAME:COUNTDOEN",
+            buildResponse({
+              time: minutes + ":" + seconds,
+            })
+          );
+          socket.emit(
+            "INGAME:COUNTDOEN",
+            buildResponse({
+              time: minutes + ":" + seconds,
+            })
+          );
+
+          if (--timer < 0) {
+            timer = duration;
+          }
+        }, 1000);
+      } catch (err) {
+        socket.emit("INGAME:COUNTDOEN", buildResponse(err));
+      }
+    });
+    socket.on("OWNER:CLOSE-ROOM", async (data) => {
+      let result;
+      try {
+        let roomObject = mapRequestBodyRoomToModelObject(data);
+        result = await GameRoomService.remove(roomObject);
+        socket.emit("ROOM:SYS-MESSAGE", buildResponse(result));
+      } catch (err) {
+        socket.emit("ROOM:SYS-MESSAGE", buildResponse(err));
+        socket.to(result._id).emit("roomRemoved", buildResponse(err));
+      }
+      console.log(result);
+    });
+    socket.on("roomList", (data) => {
+      console.log(socket.rooms);
+    });
+    socket.on("PLAYER:JOIN-ROOM", async (data) => {
       let result;
       let room;
       try {
@@ -37,65 +104,14 @@ module.exports = function (io) {
         console.log(result);
 
         socket.join(result._id);
-        socket.emit("roomMsg", buildResponse(result));
-        socket.to(result._id).emit("roomMsg", buildResponse(result));
+        socket.emit("ROOM:SYS-MESSAGE", buildResponse(result));
+        socket.to(result._id).emit("ROOM:SYS-MESSAGE", buildResponse(result));
       } catch (err) {
-        socket.emit("roomMsg", buildResponse(new InternalError(5080, err)));
+        socket.emit(
+          "ROOM:SYS-MESSAGE",
+          buildResponse(new InternalError(5080, err))
+        );
       }
-    });
-    socket.on("roomList", (data) => {
-      console.log(socket.rooms);
-    });
-    socket.on("startGame", async (data) => {
-      try {
-        //Step 1 : Random Role to player
-        let roles = await randomRoleToPlayer()
-
-
-        let timer = data.minutes * 60,
-          duration = data.minutes * 60,
-          minutes,
-          seconds;
-
-        //random location & roles
-        setInterval(function () {
-          minutes = parseInt(timer / 60, 10);
-          seconds = parseInt(timer % 60, 10);
-
-          minutes = minutes < 10 ? "0" + minutes : minutes;
-          seconds = seconds < 10 ? "0" + seconds : seconds;
-          socket.to(data.roomId).emit(
-            "countdown",
-            buildResponse({
-              time: minutes + ":" + seconds,
-            })
-          );
-          socket.emit(
-            "countdown",
-            buildResponse({
-              time: minutes + ":" + seconds,
-            })
-          );
-
-          if (--timer < 0) {
-            timer = duration;
-          }
-        }, 1000);
-      } catch (err) {
-        socket.emit("countdown", buildResponse(err));
-      }
-    });
-    socket.on("closeRoom", async (data) => {
-      let result;
-      try {
-        let roomObject = mapRequestBodyRoomToModelObject(data);
-        result = await GameRoomService.remove(roomObject);
-        socket.emit("roomMsg", buildResponse(result));
-      } catch (err) {
-        socket.emit("roomMsg", buildResponse(err));
-        socket.to(result._id).emit("roomRemoved", buildResponse(err));
-      }
-      console.log(result);
     });
     socket.on("disconnect", async () => {
       console.log("disconnected");
